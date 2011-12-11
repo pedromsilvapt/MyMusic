18254f29064eef0a400e960d6d77e5e404cefa93
Pedro Silva <pemiolsi@gmail.com>
2011-12-11 18:44:17 +0000

    New control.

---
+++ MyMusic/KLabelToTextBox.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class KLabelToTextBox
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.KTextBox = New ComponentFactory.Krypton.Toolkit.KryptonTextBox()
+        Me.KLabel = New ComponentFactory.Krypton.Toolkit.KryptonWrapLabel()
+        Me.SuspendLayout()
+        '
+        'KTextBox
+        '
+        Me.KTextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.KTextBox.Location = New System.Drawing.Point(0, 0)
+        Me.KTextBox.Multiline = True
+        Me.KTextBox.Name = "KTextBox"
+        Me.KTextBox.Size = New System.Drawing.Size(190, 75)
+        Me.KTextBox.TabIndex = 1
+        Me.KTextBox.Visible = False
+        '
+        'KLabel
+        '
+        Me.KLabel.AutoSize = False
+        Me.KLabel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.KLabel.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.KLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(76, Byte), Integer), CType(CType(83, Byte), Integer), CType(CType(92, Byte), Integer))
+        Me.KLabel.Location = New System.Drawing.Point(0, 0)
+        Me.KLabel.Name = "KLabel"
+        Me.KLabel.Size = New System.Drawing.Size(190, 75)
+        Me.KLabel.Text = "KryptonLabel1"
+        '
+        'KLabelToTextBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.KLabel)
+        Me.Controls.Add(Me.KTextBox)
+        Me.Name = "KLabelToTextBox"
+        Me.Size = New System.Drawing.Size(190, 75)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents KTextBox As ComponentFactory.Krypton.Toolkit.KryptonTextBox
+    Friend WithEvents KLabel As ComponentFactory.Krypton.Toolkit.KryptonWrapLabel
+
+End Class
---
+++ MyMusic/KLabelToTextBox.vb
@@ -0,0 +1,61 @@
+﻿Public Class KLabelToTextBox
+
+    Private Changed As Boolean = False
+
+    Public Property Label As ComponentFactory.Krypton.Toolkit.KryptonWrapLabel
+        Get
+            Return Me.KLabel
+        End Get
+        Set(ByVal value As ComponentFactory.Krypton.Toolkit.KryptonWrapLabel)
+            Me.KLabel = value
+        End Set
+    End Property
+
+    Public Property TextBox As ComponentFactory.Krypton.Toolkit.KryptonTextBox
+        Get
+            Return Me.KTextBox
+        End Get
+        Set(ByVal value As ComponentFactory.Krypton.Toolkit.KryptonTextBox)
+            Me.KTextBox = value
+        End Set
+    End Property
+
+    Public Overrides Property Text As String
+        Get
+            Return Me.KTextBox.Text
+        End Get
+        Set(ByVal value As String)
+            Me.KTextBox.Text = value
+        End Set
+    End Property
+
+
+    Private Sub KLabel_MouseClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles KLabel.MouseClick
+        Me.KTextBox.Visible = True
+        Me.KLabel.Visible = False
+        Me.KTextBox.Focus()
+    End Sub
+
+    Private Sub KTextBox_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles KTextBox.LostFocus
+        Me.KLabel.Visible = True
+        Me.KTextBox.Visible = False
+    End Sub
+
+    Private Sub KLabel_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles KLabel.TextChanged
+        If (Changed = False) Then
+            Me.Changed = True
+            Me.KTextBox.Text = Me.KLabel.Text
+        Else
+            Me.Changed = False
+        End If
+    End Sub
+
+    Private Sub KTextBox_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles KTextBox.TextChanged
+        If (Changed = False) Then
+            Me.Changed = True
+            Me.KLabel.Text = Me.KTextBox.Text
+        Else
+            Me.Changed = False
+        End If
+    End Sub
+End Class
