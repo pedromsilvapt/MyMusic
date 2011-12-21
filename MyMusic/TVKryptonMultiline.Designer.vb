cdcab2ea91daa18b8b2ef05bc04eefa07f7dc7ef
Pedro Silva <pemiolsi@gmail.com>
2011-12-21 11:54:53 +0000

    Added new component for lyrics.

---
+++ MyMusic/TVKryptonMultiline.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TVKryptonMultiline
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.KTextBox = New ComponentFactory.Krypton.Toolkit.KryptonTextBox()
+        Me.KHeader = New ComponentFactory.Krypton.Toolkit.KryptonHeader()
+        Me.KBSRevert = New ComponentFactory.Krypton.Toolkit.ButtonSpecAny()
+        Me.KBSClear = New ComponentFactory.Krypton.Toolkit.ButtonSpecAny()
+        Me.KBSCheck = New ComponentFactory.Krypton.Toolkit.ButtonSpecAny()
+        Me.SuspendLayout()
+        '
+        'KTextBox
+        '
+        Me.KTextBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.KTextBox.Location = New System.Drawing.Point(3, 40)
+        Me.KTextBox.Multiline = True
+        Me.KTextBox.Name = "KTextBox"
+        Me.KTextBox.Size = New System.Drawing.Size(225, 194)
+        Me.KTextBox.TabIndex = 3
+        '
+        'KHeader
+        '
+        Me.KHeader.AllowButtonSpecToolTips = True
+        Me.KHeader.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.KHeader.AutoSize = False
+        Me.KHeader.ButtonSpecs.AddRange(New ComponentFactory.Krypton.Toolkit.ButtonSpecAny() {Me.KBSRevert, Me.KBSClear, Me.KBSCheck})
+        Me.KHeader.Location = New System.Drawing.Point(3, 3)
+        Me.KHeader.Name = "KHeader"
+        Me.KHeader.Size = New System.Drawing.Size(225, 31)
+        Me.KHeader.TabIndex = 6
+        Me.KHeader.Values.Description = ""
+        Me.KHeader.Values.Heading = "Krypton Header Text"
+        Me.KHeader.Values.Image = Nothing
+        '
+        'KBSRevert
+        '
+        Me.KBSRevert.ToolTipBody = "Reverter"
+        Me.KBSRevert.Type = ComponentFactory.Krypton.Toolkit.PaletteButtonSpecStyle.ArrowLeft
+        Me.KBSRevert.UniqueName = "83BA0203CA874CE08DAA026EAF128B35"
+        '
+        'KBSClear
+        '
+        Me.KBSClear.ToolTipBody = "Limpar"
+        Me.KBSClear.Type = ComponentFactory.Krypton.Toolkit.PaletteButtonSpecStyle.Close
+        Me.KBSClear.UniqueName = "417DB2DF465241B03E8357D350595D9B"
+        '
+        'KBSCheck
+        '
+        Me.KBSCheck.Checked = ComponentFactory.Krypton.Toolkit.ButtonCheckState.Unchecked
+        Me.KBSCheck.Type = ComponentFactory.Krypton.Toolkit.PaletteButtonSpecStyle.PinVertical
+        Me.KBSCheck.UniqueName = "AC6BD61A892A46F24B9B5689F25CC1B9"
+        Me.KBSCheck.Visible = False
+        '
+        'TVKryptonMultiline
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.KHeader)
+        Me.Controls.Add(Me.KTextBox)
+        Me.Name = "TVKryptonMultiline"
+        Me.Size = New System.Drawing.Size(231, 237)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents KTextBox As ComponentFactory.Krypton.Toolkit.KryptonTextBox
+    Friend WithEvents KHeader As ComponentFactory.Krypton.Toolkit.KryptonHeader
+    Friend WithEvents KBSRevert As ComponentFactory.Krypton.Toolkit.ButtonSpecAny
+    Friend WithEvents KBSClear As ComponentFactory.Krypton.Toolkit.ButtonSpecAny
+    Friend WithEvents KBSCheck As ComponentFactory.Krypton.Toolkit.ButtonSpecAny
+
+End Class
---
+++ MyMusic/TVKryptonMultiline.vb
@@ -0,0 +1,210 @@
+﻿Public Class TVKryptonMultiline
+    Private TextValue1 As String = ""
+    Private TextValue2 As String = ""
+    Private Value2Editable As Boolean = False
+    Private AutoChangeTo1 As Boolean = True
+    Private _RevertText As String
+
+    Private _ShowClear As Boolean = True
+
+    Private _ShowRevert As Boolean = True
+
+    Public Property RevertText As String
+        Get
+            Return Me._RevertText
+        End Get
+        Set(ByVal value As String)
+            Me._RevertText = value
+
+            Me.KBSRevert.Visible = ((Me.TextBoxValue <> Me.RevertText) And (Me._ShowRevert = True))
+        End Set
+    End Property
+
+    Public Property ShowClear As Boolean
+        Get
+            Return Me._ShowClear
+        End Get
+        Set(ByVal value As Boolean)
+            Me._ShowClear = value
+
+            Me.KBSClear.Visible = ((Me.TextBoxValue <> "") And (Me._ShowClear))
+        End Set
+    End Property
+
+    Public Property ShowRevert As Boolean
+        Get
+            Return Me._ShowRevert
+        End Get
+        Set(ByVal value As Boolean)
+            Me._ShowRevert = value
+
+            Me.KBSRevert.Visible = ((Me.KTextBox.Text <> Me.RevertText) And (Me._ShowRevert))
+        End Set
+    End Property
+
+    Public Property TextBoxX As Single
+        Get
+            Return Me.KTextBox.Location.X
+        End Get
+        Set(ByVal value As Single)
+            Me.KTextBox.Location = New Point(value, Me.KTextBox.Location.Y)
+        End Set
+    End Property
+
+    Public Property TextBoxWidth As Single
+        Get
+            Return Me.KTextBox.Size.Width
+        End Get
+        Set(ByVal value As Single)
+            Me.KTextBox.Size = New Size(value, Me.KTextBox.Size.Height)
+        End Set
+    End Property
+
+    Public Property TextBoxValue1 As String
+        Get
+            Return Me.TextValue1
+        End Get
+        Set(ByVal value As String)
+            If (Me.KChecked) Then
+                Me.TextValue1 = value
+            Else
+                Me.KTextBox.Text = value
+            End If
+        End Set
+    End Property
+
+    Public Property TextBoxValue2 As String
+        Get
+            Return Me.TextValue2
+        End Get
+        Set(ByVal value As String)
+            If (Me.KChecked) Then
+                Me.KTextBox.Text = value
+            Else
+                Me.TextValue2 = value
+            End If
+        End Set
+    End Property
+
+    Public Property TextBoxValue As String
+        Get
+            Return Me.KTextBox.Text
+        End Get
+        Set(ByVal value As String)
+            Me.KTextBox.Text = value
+        End Set
+    End Property
+
+    Public Property LabelText As String
+        Get
+            Return Me.KHeader.Text
+        End Get
+        Set(ByVal value As String)
+            Me.KHeader.Text = value
+        End Set
+    End Property
+
+    Public Property ValueTwoEditable As Boolean
+        Get
+            Return Me.Value2Editable
+        End Get
+        Set(ByVal value As Boolean)
+            Me.Value2Editable = value
+        End Set
+    End Property
+
+    Public Property AutoChangeToFirst As Boolean
+        Get
+            Return Me.AutoChangeTo1
+        End Get
+        Set(ByVal value As Boolean)
+            Me.AutoChangeTo1 = value
+        End Set
+    End Property
+
+    Public Property CheckBoxVisible As Boolean
+        Get
+            Return Me.KBSCheck.Visible
+        End Get
+        Set(ByVal value As Boolean)
+            Me.KBSCheck.Visible = value
+        End Set
+    End Property
+
+    Public Property KChecked As Boolean
+        Get
+            If Me.KBSCheck.Checked = ComponentFactory.Krypton.Toolkit.ButtonCheckState.Checked Then
+                Return True
+            Else
+                Return False
+            End If
+        End Get
+        Set(ByVal value As Boolean)
+            If value = True Then
+                Me.KBSCheck.Checked = ComponentFactory.Krypton.Toolkit.ButtonCheckState.Checked
+            Else
+                Me.KBSCheck.Checked = ComponentFactory.Krypton.Toolkit.ButtonCheckState.Unchecked
+            End If
+        End Set
+    End Property
+
+    Public Property LabelColor As Color
+        Get
+            Return Me.KHeader.StateCommon.Content.ShortText.Color1
+        End Get
+        Set(ByVal value As Color)
+            Me.KHeader.StateCommon.Content.ShortText.Color1 = value
+        End Set
+    End Property
+
+    Public Event AnyTextChanged(ByVal sender As Object, ByVal e As System.EventArgs)
+    Public Event Text1Changed(ByVal sender As Object, ByVal e As System.EventArgs)
+    Public Event Text2Changed(ByVal sender As Object, ByVal e As System.EventArgs)
+
+    Private Sub KTextBox_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles KTextBox.TextChanged
+        Me.KBSClear.Visible = ((Me.TextBoxValue <> "") And (Me._ShowClear))
+
+        If (Me.KChecked = True) Then
+            If (Me.Value2Editable = True) Then
+                Me.TextValue2 = Me.KTextBox.Text
+                RaiseEvent Text2Changed(sender, e)
+            Else
+                Me.TextValue1 = Me.KTextBox.Text
+                Me.KBSCheck.Checked = False
+
+                Me.KBSRevert.Visible = ((Me.TextBoxValue <> Me.RevertText) And (Me.ShowRevert = True))
+
+                RaiseEvent Text1Changed(sender, e)
+            End If
+        Else
+            Me.TextValue1 = Me.KTextBox.Text
+
+            Me.KBSRevert.Visible = ((Me.TextBoxValue <> Me.RevertText) And (Me.ShowRevert = True))
+
+            RaiseEvent Text1Changed(sender, e)
+        End If
+        RaiseEvent AnyTextChanged(sender, e)
+    End Sub
+
+    Private Sub KCheckBox_CheckedChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles KBSCheck.Click
+        If (Me.KChecked = True) Then
+            Me.KTextBox.Text = Me.TextValue2
+        Else
+            Me.KTextBox.Text = Me.TextValue1
+        End If
+    End Sub
+
+    Private Sub KCheckBox_VisibleChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles KBSCheck.ButtonSpecPropertyChanged
+        If (Me.AutoChangeTo1 = True And Me.KBSCheck.Visible = False) Then
+            Me.KChecked = False
+        End If
+    End Sub
+
+    Private Sub btnsc_revert_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles KBSRevert.Click
+        Me.TextBoxValue = Me.RevertText
+    End Sub
+
+    Private Sub KBSClear_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles KBSClear.Click
+        Me.TextBoxValue = ""
+    End Sub
+End Class
